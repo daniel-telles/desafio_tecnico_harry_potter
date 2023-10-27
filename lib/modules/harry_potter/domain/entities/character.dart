3fb943e9512960c2f3274fe7c1c04609886ebfd1
daniel-telles <danieltelles7@gmail.com>
2023-10-27 19:15:52 -0300

    character entity

---
+++ lib/modules/harry_potter/domain/entities/character.dart
@@ -0,0 +1,64 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'wand.dart';
+
+class Character {
+  final String? name;
+  final String? species;
+  final String? gender;
+  final String? house;
+  final String? dateOfBirth;
+  final bool? wizard;
+  final String? ancestry;
+  final String? eyeColour;
+  final String? hairColour;
+  final List<Wand>? wand;
+  final String? patronus;
+  final bool? hogwartsStudent;
+  final bool? hogwartsStaff;
+  final String? actor;
+  final bool? alive;
+  final String? image;
+
+  Character({
+    this.name,
+    this.species,
+    this.gender,
+    this.house,
+    this.dateOfBirth,
+    this.wizard,
+    this.ancestry,
+    this.eyeColour,
+    this.hairColour,
+    this.wand,
+    this.patronus,
+    this.hogwartsStudent,
+    this.hogwartsStaff,
+    this.actor,
+    this.alive,
+    this.image,
+  });
+
+
+  factory Character.fromJson(Map<String, dynamic> json) {
+    return Character(
+      name: json["name"],
+      species: json["species"],
+      gender: json["gender"],
+      house: json["house"],
+      dateOfBirth: json["dateOfBirth"],
+      wizard: json["wizard"],
+      ancestry: json["ancestry"],
+      eyeColour: json["eyeColour"],
+      hairColour: json["hairColour"],
+      wand: json["wand"],
+      patronus: json["patronus"],
+      hogwartsStudent: json["hogwartsStudent"],
+      hogwartsStaff: json["hogwartsStaff"],
+      actor: json["actor"],
+      alive: json["alive"],
+      image: json["image"],
+
+
+    );
+  }
+}
